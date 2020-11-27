8f83920b27f4f85ee669371c7a5fa5f6c217d301
Amelia Aronsohn <squirrel@wearing.black>
2020-11-27 15:29:32 -0800

    Move to bash and allow for a config file

--- update_transmission_blocklists.sh
+++ update_transmission_blocklists.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cd "$HOME/.config/transmission-daemon/blocklists"
+BLOCKLIST_DIR="$HOME/.config/transmission-daemon/blocklists"
+
+## Put Overrides for the above in this file
+source "$HOME/.config/blocklists.conf"
+
+cd "$BLOCKLIST_DIR" || exit 1
 
 curl -L \
 	http://list.iblocklist.com/?list=bt_level[1-3] \
 	-o "bt_level#1.gz"
-gunzip -f *.gz
+
+gunzip -f ./*.gz
 
 killall -HUP transmission-daemon
